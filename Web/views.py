7655496b5cefeea3941e1a373930693ea941343e
Mert Dursun <mertdursun@sabanciuniv.edu>
2022-04-07 21:25:49 +0300

    Encountered Problems Creating Users API

--- Web/views.py
+++ Web/views.py
@@ -2,12 +2,22 @@ from django.shortcuts import render
 from rest_framework import generics
 
 from .models import NFT, NFTCollection, User
-from .serializers import NFTSerializer
+from .serializers import NFTCollectionSerializer, NFTSerializer, UserSerializer
 
 
 class NFTListView(generics.ListAPIView):
     queryset = NFT.objects.all()
     serializer_class = NFTSerializer
+
+
+class NFTCollectionListView(generics.ListAPIView):
+    queryset = NFTCollection.objects.all()
+    serializer_class = NFTCollectionSerializer
+
+
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 # Create your views here.
