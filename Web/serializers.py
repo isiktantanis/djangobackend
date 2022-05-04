db65f4d99d986809f494ce0dee699d0e48235813
Mert Dursun <mertdursun@sabanciuniv.edu>
2022-05-04 23:18:45 +0300

    Changed Fields of Serializers to All

--- Web/serializers.py
+++ Web/serializers.py
@@ -1,29 +1,26 @@
+from djoser.serializers import UserCreateSerializer
 from rest_framework import serializers
 
-from .models import NFT, NFTCollection, NFTCollectionCategory, User, UserFavoritedNFT, UserWatchListedNFTCollection
-from djoser.serializers import UserCreateSerializer
+from .models import (
+    NFT,
+    NFTCollection,
+    NFTCollectionCategory,
+    User,
+    UserFavoritedNFT,
+    UserWatchListedNFTCollection,
+)
+
 
 class UserCreateSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
         model = User
         fields = "__all__"
 
+
 class NFTSerializer(serializers.ModelSerializer):
     class Meta:
         model = NFT
-        fields = [
-            "UID",
-            "index",
-            "name",
-            "description",
-            "metaDataType",
-            "dataLink",
-            "collectionName",
-            "creator",
-            "currentOwner",
-            "marketStatus",
-            "nftFile"
-        ]
+        fields = "__all__"
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -31,20 +28,24 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = "__all__"
 
+
 class NFTCollectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = NFTCollection
-        fields = ["name", "description", "owner", "category", "collectionImage"]
+        fields = "__all__"
+
 
 class NFTCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = NFTCollectionCategory
-        fields = ["name"]
+        fields = "__all__"
+
 
 class UserFavoritedNFTSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserFavoritedNFT
         fields = "__all__"
+
 
 class UserWatchListedNFTCollectionSerializer(serializers.ModelSerializer):
     class Meta:
