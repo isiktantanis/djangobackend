085f1bdc75580d5c92a95034f105b011f009381a
Elif Demiralp <elifdemiralp@sabanciuniv.edu>
2022-05-06 18:38:35 +0300

    CORS

--- NFTMarketBackend/settings.py
+++ NFTMarketBackend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "mptt",
     "rest_framework",
     "djoser",
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = "NFTMarketBackend.urls"
