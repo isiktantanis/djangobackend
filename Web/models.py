50a1062c64c64fb0eb658acdfa4a1040664299c1
Işıktan <isiktantanis@gmail.com>
2022-05-07 17:22:30 +0300

    simple bug fix

--- Web/models.py
+++ Web/models.py
@@ -45,35 +45,35 @@ class OverwriteStorage(FileSystemStorage):
 
 
 class NFT(MPTTModel):
-        UID = models.TextField(verbose_name=_("Unique ID"))
-        index = models.IntegerField(verbose_name=_("index"))
-        name = models.CharField(max_length=128, verbose_name=_("Name"))
-        description = models.TextField(verbose_name=_("Description"), null=True, blank=True)
-        metaDataType = models.CharField(
-            verbose_name=_("Metadata Type"),
-            max_length=5,
-            choices=[("video", "video"), ("audio", "audio"), ("image", "image")],
-        )
-        dataLink = models.URLField(verbose_name=_("Data Link"))
-        # take this link and move it to database and create another link
-        # TODO: [NFTMAR-130] MAKE USERS DELETABLE WITHOUT EFFECTING NFTS
-        collectionName = models.ForeignKey(
-            "NFTCollection", verbose_name=_("Collection Name"), on_delete=models.SET_NULL, null=True
-        )
-        creator = models.ForeignKey(
-            "User", related_name="creator", verbose_name=_("Creator"), on_delete=models.SET("USER_DELETED")
-        )
-        currentOwner = models.ForeignKey("User", verbose_name=_("Current Owner"), on_delete=models.SET("USER_DELETED"))
-        marketStatus = models.IntegerField(
-            verbose_name=_("Market Status"), default=0, choices=[(0, "Not On Market"), (1, "Not On Sale"), (2, "On Sale")]
-        )
-        nftFile = models.FileField(
-            upload_to="nfts/", blank=True, null=True, storage=OverwriteStorage(), verbose_name=_("File")
-        )
-        numLikes = models.IntegerField(default=0, verbose_name=_("Number of Likes"))
-        parent = TreeForeignKey(
-            "self", on_delete=models.CASCADE, null=True, blank=True, related_name="children", editable=False
-        )
+    UID = models.TextField(verbose_name=_("Unique ID"))
+    index = models.IntegerField(verbose_name=_("index"))
+    name = models.CharField(max_length=128, verbose_name=_("Name"))
+    description = models.TextField(verbose_name=_("Description"), null=True, blank=True)
+    metaDataType = models.CharField(
+        verbose_name=_("Metadata Type"),
+        max_length=5,
+        choices=[("video", "video"), ("audio", "audio"), ("image", "image")],
+    )
+    dataLink = models.URLField(verbose_name=_("Data Link"))
+    # take this link and move it to database and create another link
+    # TODO: [NFTMAR-130] MAKE USERS DELETABLE WITHOUT EFFECTING NFTS
+    collectionName = models.ForeignKey(
+        "NFTCollection", verbose_name=_("Collection Name"), on_delete=models.SET_NULL, null=True
+    )
+    creator = models.ForeignKey(
+        "User", related_name="creator", verbose_name=_("Creator"), on_delete=models.SET("USER_DELETED")
+    )
+    currentOwner = models.ForeignKey("User", verbose_name=_("Current Owner"), on_delete=models.SET("USER_DELETED"))
+    marketStatus = models.IntegerField(
+        verbose_name=_("Market Status"), default=0, choices=[(0, "Not On Market"), (1, "Not On Sale"), (2, "On Sale")]
+    )
+    nftFile = models.FileField(
+        upload_to="nfts/", blank=True, null=True, storage=OverwriteStorage(), verbose_name=_("File")
+    )
+    numLikes = models.IntegerField(default=0, verbose_name=_("Number of Likes"))
+    parent = TreeForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True, related_name="children", editable=False
+    )
 
     def __str__(self):
         return self.name
